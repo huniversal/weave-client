1240163047eb6d280d12a53d17d51b5c409b8110
huniversal <hunivers0523@naver.com>
2026-03-31 23:42:28 +0900

    fix: 누락된 디자인 토큰 복구

--- src/renderer/styles/token/font.css.ts
+++ src/renderer/styles/token/font.css.ts
@@ -14,5 +14,12 @@ export const font = {
     regular: "400",
     medium: "500",
     semibold: "600"
+  },
+  lineHeight: {
+    tight: "120%",
+    normal: "130%"
+  },
+  letterSpacing: {
+    tight: "-0.025em"
   }
 } as const;
